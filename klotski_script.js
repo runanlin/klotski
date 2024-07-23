8a0d398591711ae16f2c8002deb330a328607288
runanlin <107426955+runanlin@users.noreply.github.com>
2024-07-23 17:47:39 -0500

    Added Level 3

--- klotski_script.js
+++ klotski_script.js
@@ -45,16 +45,16 @@ window.addEventListener('load', (event) => {
             ],
             // Level 3 setup
             [
-                { row: 0, col: 0, width: 2, height: 1 },
-                { row: 0, col: 2, width: 2, height: 1 },
-                { row: 1, col: 0, width: 2, height: 1 },
-                { row: 2, col: 0, width: 2, height: 1 },
-                { row: 2, col: 2, width: 2, height: 1 },
-                { row: 3, col: 0, width: 1, height: 1 },
-                { row: 3, col: 1, width: 1, height: 1 },
-                { row: 3, col: 2, width: 2, height: 2, color: '#545e56'},
-                { row: 4, col: 0, width: 1, height: 1 },
-                { row: 4, col: 1, width: 1, height: 1 },
+                { row: 0, col: 0, width: 1, height: 2 },
+                { row: 0, col: 1, width: 2, height: 2, color: '#545e56' },
+                { row: 0, col: 3, width: 1, height: 1 },
+                { row: 1, col: 3, width: 1, height: 1 },
+                { row: 2, col: 0, width: 1, height: 2 },
+                { row: 2, col: 1, width: 2, height: 1 },
+                { row: 2, col: 3, width: 1, height: 1 },
+                { row: 3, col: 1, width: 1, height: 2 },
+                { row: 3, col: 2, width: 1, height: 2 },
+                { row: 3, col: 3, width: 1, height: 1 },
             ],
             // Level 4 setup
             [
